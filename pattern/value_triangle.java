ab7f961cd961898cef350d3fb1bee8e7da13c41b
SUBHADIP PATRA <drsahebsubhadip@gmail.com>
2026-03-17 21:03:41 +0530

    added value_triangle

---
+++ pattern/value_triangle.java
@@ -0,0 +1,21 @@
+package pattern;
+import java.util.Scanner;
+
+public class value_triangle {
+    public static void main(String[]args){
+        Scanner myobj = new Scanner(System.in);
+        int n = myobj.nextInt();
+        for (int i = 1 ; i <=n ;i ++){
+            for (int j=1;j<=i;j++ ){
+
+                System.out.print(i);
+            }
+            System.out.println();
+
+
+        }
+
+
+
+    }
+}
